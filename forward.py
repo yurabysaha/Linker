f4e055806793b5704fe9a2b88cc3cee13c020510
yurabysaha <yurabysaha@gmail.com>
2017-04-25 10:47:38 +0300

    finish forward

--- forward.py
+++ forward.py
@@ -35,15 +35,24 @@ class Forward:
         self.chrome.find_element_by_id('login-submit').click()
 
     def resend_message(self):
-        people = user.candidate_for_message()
+        people = user.candidate_for_forward()
+        if not people:
+            self.text.insert('end', "Nobody to send final messages :(\n")
+            self.text.see('end')
+            self.chrome.close()
+            return
         self.chrome.get(url='https://www.linkedin.com/messaging/')
         time.sleep(5)
         search_field = self.chrome.find_element_by_xpath(".//input[@id='search-conversations']")
         for name in people:
+            search_field.clear()
             search_field.send_keys(name)
+            search_field.send_keys(Keys.ENTER)
             time.sleep(5)
             try:
                 first_item_list = self.chrome.find_element_by_xpath(".//div[@class='msg-conversations-container']//li[1]")
+                if first_item_list.get_attribute("class") == "msg-premium-mailboxes__mailbox":
+                    first_item_list = self.chrome.find_element_by_xpath(".//div[@class='msg-conversations-container']//li[2]")
                 first_item_list.click()
             except NoSuchElementException:
                 self.text.insert('end', "Not found : %s\n" % name)
@@ -52,10 +61,10 @@ class Forward:
             time.sleep(2)
             person_name = self.chrome.find_element_by_xpath(".//dt[@class='truncate']/h3").text
             time.sleep(1)
-            if person_name == name:
-                count_messages = len(self.chrome.find_element_by_xpath(".//div[contains(@class, 'message-bubble')]"))
-                if count_messages > 1:
-                    user.finish(name)
+            if person_name == name[0]:
+                count_messages = self.chrome.find_elements_by_xpath(".//div[contains(@class, 'message-bubble')]")
+                if len(count_messages) > 1:
+                    user.finish(name[0])
                 else:
                     # Send forward message
                     text_field = self.chrome.find_element_by_xpath(".//textarea")
@@ -66,10 +75,10 @@ class Forward:
                         text_field.send_keys(Keys.SHIFT + Keys.ENTER)
                         time.sleep(1)
                     text_field.submit()
-                    self.text.insert('end', "Message was sent to: %s\n" % name[0])
+                    self.text.insert('end', "Final message was sent to: %s\n" % name[0])
                     self.text.see('end')
                     user.send_second_message(name[0])
                     time.sleep(31)
-                    # Push data base
-
-
+        self.text.insert('end', "Yonchi send all final messages, Yeeeeee!\n")
+        self.text.see('end')
+        self.chrome.close()
