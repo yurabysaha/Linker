de5c92ecf99fce01ba559b762d000be94579b6c8
yurabysaha <yurabysaha@gmail.com>
2017-05-08 15:01:47 +0300

    feed add

---
+++ feed.py
@@ -0,0 +1,46 @@
+from ConfigParser import RawConfigParser
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+import user
+from base import BaseMethod
+from results import Results
+
+
+class Feed(BaseMethod):
+    def __init__(self):
+        BaseMethod.__init__(self)
+
+        self.login()
+        self.feed_news()
+
+    def feed_news(self):
+        keys = ['looking for', 'need full-stack developer', 'looking for full-stack developer', 'hiring developers',
+                'remote developer', 'need ios', 'need android']
+        find = True
+        count = 0
+        results = Results()
+        results.create_file()
+        while find:
+            self.chrome.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(5)
+            blocks = self.chrome.find_elements_by_xpath('.//*[@class="core-rail"]/div[@class="ember-view"]/div')
+            for i in blocks[count:]:
+                try:
+                    t = i.find_element_by_xpath('.//article//p').text
+                    for k in keys:
+                        if k in t:
+                            print t
+                            href = i.find_element_by_xpath('.//article//a').get_attribute('href')
+                            print href
+                            results.update_file(t, href)
+                            break
+                except:
+                    pass
+            if count == len(blocks):
+                find = False
+            else:
+                count = len(blocks)
+        self.chrome.close()
