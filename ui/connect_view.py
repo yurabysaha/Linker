95e2808335f6c10ea6790133d28ff9d789f186d6
yurabysaha <yurabysaha@gmail.com>
2017-05-08 16:05:51 +0300

    ui for sales_connect

--- ui/connect_view.py
+++ ui/connect_view.py
@@ -1,5 +1,8 @@
 import Tkinter as tk
 import threading
+import tkMessageBox
+from ConfigParser import RawConfigParser
+
 import user
 
 from connect import Connect
@@ -16,6 +19,26 @@ class ConnectView:
         tk.Label(self.body, bg='#e6e6e6', text='Today     |     All ').place(x=130, y=10)
         tk.Label(self.body, bg='#e6e6e6', text='Send requests  ').place(x=10, y=30)
         tk.Label(self.body, bg='#e6e6e6', text=' %s     |     %s ' % (data[0][0], data[1][0])).place(x=154, y=30)
+
+        tk.Label(self.body, bg='#e6e6e6', text='Message Text For Sales Connect').place(x=10, y=60)
+        self.sales_mess_entry = tk.Text(self.body, width=46, height=14)
+        self.sales_mess_entry.place(x=10, y=80)
+
+        config = RawConfigParser()
+        config.read('../config.ini')
+        self.sales_mess_entry.insert(1.0, config.get('main', 'sales_message_text'))
+
+        self.update_mess_btn = tk.Button(self.body,
+                                         text='Update info',
+                                         fg='#ffffff',
+                                         bg='#214312', activebackground='#e6e6e6',
+                                         borderwidth=0,
+                                         highlightthickness=0,
+                                         width=18, height=2)
+
+        self.update_mess_btn.bind("<Button-1>", self.update_message_text)
+        self.update_mess_btn.place(x=130, y=320)
+
         self.connect_btn = tk.Button(self.body,
                                      text='Start Simple Connect',
                                      fg='#ffffff',
@@ -51,3 +74,13 @@ class ConnectView:
             t = threading.Thread(target=Sales, args=(self.text,))
             t.start()
             self.text.insert('end', "Start Sales Connect people\n")
+
+    def update_message_text(self, event):
+        config = RawConfigParser()
+        config.read('../config.ini')
+        config.set('main', 'sales_message_text', str(self.sales_mess_entry.get(1.0, 'end')))
+
+        with open('../config.ini', 'w') as f:
+            config.write(f)
+
+        tkMessageBox.showinfo("Updated", "Message text update successful")
