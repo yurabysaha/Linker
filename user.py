bc4faad2aece45e866eea547f517a3d1a00f7aca
yurabysaha <yurabysaha@gmail.com>
2017-06-22 14:36:36 +0300

    Local version

--- user.py
+++ user.py
@@ -6,102 +6,76 @@ from ConfigParser import RawConfigParser
 from datetime import date
 from time import strftime
 
-import psycopg2
-import psycopg2.extras
-
 today = date(2013, 11, 1)
-# c = db.connect(database="../db")
-# cu = c.cursor()
-# try:
-#     cu.executescript("""
-#           CREATE TABLE users (
-#             id INTEGER PRIMARY KEY AUTOINCREMENT,
-#             bot_name VARCHAR,
-#             name VARCHAR,
-#             link VARCHAR,
-#             accept_connect BOOLEAN DEFAULT 0,
-#             accept_date DATE,
-#             send_message BOOLEAN DEFAULT 0,
-#             send_date  DATE,
-#             second_message BOOLEAN DEFAULT 0,
-#             second_message_date DATE,
-#             finished BOOLEAN DEFAULT 0
-#             ,created_at datetime  NOT NULL  DEFAULT current_timestamp
-#             ,updated_at datetime  NOT NULL  DEFAULT current_timestamp
-#             );
-#
-#         -- trigger (updated_at)
-#         CREATE TRIGGER tg_users_updated_at
-#         AFTER UPDATE
-#         ON users FOR EACH ROW
-#         BEGIN
-#           UPDATE users SET updated_at = current_timestamp
-#             WHERE id = old.id;
-#         END;""")
-#
-# except db.DatabaseError, x:
-#     print "DB Error: ", x
-# c.commit()
-# c.close()
+c = db.connect(database="../db")
+cu = c.cursor()
+try:
+    cu.executescript("""
+          CREATE TABLE users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            bot_name VARCHAR,
+            name VARCHAR,
+            link VARCHAR,
+            accept_connect BOOLEAN DEFAULT 0,
+            accept_date DATE,
+            send_message BOOLEAN DEFAULT 0,
+            send_date  DATE,
+            second_message BOOLEAN DEFAULT 0,
+            second_message_date DATE,
+            finished BOOLEAN DEFAULT 0
+            ,created_at datetime  NOT NULL  DEFAULT current_timestamp
+            ,updated_at datetime  NOT NULL  DEFAULT current_timestamp
+            );
 
+        -- trigger (updated_at)
+        CREATE TRIGGER tg_users_updated_at
+        AFTER UPDATE
+        ON users FOR EACH ROW
+        BEGIN
+          UPDATE users SET updated_at = current_timestamp
+            WHERE id = old.id;
+        END;""")
 
-# def db_decorator(func):
-#     def a_wrapper_accepting_arbitrary_arguments(self, *args, **kwargs):
-#         value = None
-#         if self.db_type == 1 or self.db_type == 3:
-#             self.con = db.connect(database="../db")
-#             self.cur = self.con.cursor()
-#             value = func(self, *args, **kwargs)
-#         if self.db_type == 2 or self.db_type == 3:
-#             conn_string = "host='5.9.51.242' port='5432' dbname='db_botnet' user='postgres' password='pomason3'"
-#             self.con = psycopg2.connect(conn_string)
-#             self.cur = self.con.cursor(cursor_factory = psycopg2.extras.DictCursor)
-#             remote = func(self, *args, **kwargs)
-#             value = remote
-#         return value
-#     return a_wrapper_accepting_arbitrary_arguments
+except db.DatabaseError, x:
+    print "DB Error: ", x
+c.commit()
+c.close()
 
-# 1q2w!@QW
-# alexandervojak@gmail.com
-# https://www.linkedin.com/search/results/people/?facetGeoRegion=%5B%22gb%3A0%22%5D&facetIndustry=%5B%2296%22%5D&facetNetwork=%5B%22S%22%5D&origin=FACETED_SEARCH&title=CEO
-# DB_TABLE = 'tb_eq_users'
 DB_TABLE = 'users'
+
+
 class User:
     def __init__(self):
         config = RawConfigParser()
         config.read('../config.ini')
         self.bot_name = (config.get('main', 'email')).lower()
-        self.db_type = config.getint('main', 'db_type')
-        conn_string = "host='5.9.51.242' port='5432' dbname='db_botnet' user='postgres' password='pomason3'"
-        self.con = psycopg2.connect(conn_string)
-        self.cur = self.con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        self.con = db.connect(database="../db")
+        self.cur = self.con.cursor()
 
-   # @db_decorator
     def create(self, name, link):
         # return 0 - if user already in database (added)
         # return 1 - if we add new user success
         # self.cur.execute("insert into users (bot_name, name, link) values (?, ?, ?);", (self.bot_name, name, link,))
-        self.cur.execute("SELECT * FROM "+DB_TABLE+" WHERE bot_name=%s AND name=%s AND link=%s;", (self.bot_name, name, link,))
+        self.cur.execute("SELECT * FROM "+DB_TABLE+" WHERE bot_name=? AND name=? AND link=?;", (self.bot_name, name, link,))
         data = self.cur.fetchall()
         if data:
             resp = 0
         else:
-            self.cur.execute("insert into "+DB_TABLE+" (bot_name, name, link) values (%s, %s, %s);", (self.bot_name, name, link,))
+            self.cur.execute("insert into "+DB_TABLE+" (bot_name, name, link) values (?, ?, ?);", (self.bot_name, name, link,))
             self.con.commit()
             resp = 1
         self.con.close()
         return resp
 
-   # @db_decorator
     def accept(self, name):
         # :return
         # 1 - Статус змінено на True
         # 2 - Такого користувача не знайдено в базі
         # 3 - У користувача вже є статус True
-        self.cur.execute("SELECT * FROM "+DB_TABLE+" WHERE name=%s AND bot_name=%s;", (name, self.bot_name,))
+        self.cur.execute("SELECT * FROM "+DB_TABLE+" WHERE name=? AND bot_name=?;", (name, self.bot_name,))
         user = self.cur.fetchone()
         if user and not user[4]:
-            query = "UPDATE "+DB_TABLE+" set accept_connect= TRUE, accept_date=%s WHERE name=%s AND bot_name=%s"
+            query = "UPDATE "+DB_TABLE+" set accept_connect=1, accept_date=? WHERE name=? AND bot_name=?"
             self.cur.execute(query, (date.today(), name, self.bot_name,))
             self.con.commit()
             self.con.close()
@@ -111,87 +85,76 @@ class User:
                 return 2
             return 3
 
-   # @db_decorator
     def send_message(self, name):
-        self.cur.execute("UPDATE "+DB_TABLE+" set send_message=TRUE, send_date=%s where name=%s AND bot_name=%s", (date.today(), name, self.bot_name,))
+        self.cur.execute("UPDATE "+DB_TABLE+" set send_message=1, send_date=? where name=? AND bot_name=?", (date.today(), name, self.bot_name,))
         self.con.commit()
         self.con.close()
 
-   # @db_decorator
     def get_day_counter(self):
-        self.cur.execute("SELECT id FROM "+DB_TABLE+" WHERE created_at >=%s AND bot_name=%s;", (date.today(), self.bot_name,))
+        self.cur.execute("SELECT id FROM "+DB_TABLE+" WHERE created_at >=? AND bot_name=?;", (date.today(), self.bot_name,))
         users = self.cur.fetchall()
         self.con.close()
         return len(users)
 
-    #@db_decorator
     def get_today_connection_results(self):
          self.cur.execute("SELECT name, accept_connect, send_message, second_message, finished FROM "+DB_TABLE+" "
-                            "WHERE updated_at >=%s AND bot_name=%s;", (date.today(), self.bot_name,))
+                            "WHERE updated_at >=? AND bot_name=?;", (date.today(), self.bot_name,))
          users = self.cur.fetchall()
          self.con.close()
          return date.today(), users
 
-   # @db_decorator
     def get_all_connection_results(self):
         self.cur.execute(
-            "SELECT to_char(created_at, 'YYYY-MM-DD'), name, accept_connect, send_message, second_message, finished FROM "+DB_TABLE+" WHERE bot_name=%s ORDER BY created_at desc;", (self.bot_name,))
+            "SELECT to_char(created_at, 'YYYY-MM-DD'), name, accept_connect, send_message, second_message, finished FROM "+DB_TABLE+" WHERE bot_name=? ORDER BY created_at desc;", (self.bot_name,))
         users = self.cur.fetchall()
         self.con.close()
         return users
 
-   # @db_decorator
     def candidate_for_message(self):
-        self.cur.execute("SELECT name FROM "+DB_TABLE+" WHERE accept_connect=%s AND send_message=%s AND bot_name=%s AND finished=%s;",
+        self.cur.execute("SELECT name FROM "+DB_TABLE+" WHERE accept_connect=? AND send_message=? AND bot_name=? AND finished=?;",
                             (True, False, self.bot_name, False,))
         users = self.cur.fetchall()
         self.con.close()
         return users
 
-   # @db_decorator
     def candidate_for_review(self):
-        self.cur.execute("SELECT name FROM "+DB_TABLE+" WHERE accept_connect=%s AND send_message=%s  AND bot_name=%s AND finished=%s;",
+        self.cur.execute("SELECT name FROM "+DB_TABLE+" WHERE accept_connect=? AND send_message=? AND bot_name=? AND finished=?;",
                             (False, False, self.bot_name, False,))
-        users =self.cur.fetchall()
+        users = self.cur.fetchall()
         self.con.close()
         return users
 
-   # @db_decorator
     def count_connections(self):
-        self.cur.execute("SELECT COUNT(*) FROM "+DB_TABLE+" WHERE bot_name = %s;", (self.bot_name,))
+        self.cur.execute("SELECT COUNT(*) FROM "+DB_TABLE+" WHERE bot_name=?;", (self.bot_name,))
         all_count = self.cur.fetchone()
 
-        self.cur.execute("SELECT COUNT(*) FROM "+DB_TABLE+" WHERE created_at >=DATE('now') AND bot_name=%s;", (self.bot_name,))
+        self.cur.execute("SELECT COUNT(*) FROM "+DB_TABLE+" WHERE created_at >=DATE('now') AND bot_name=?;", (self.bot_name,))
         today_count = self.cur.fetchone()
         self.con.close()
         return today_count, all_count
 
-   # @db_decorator
     def count_accepted(self):
-        self.cur.execute("SELECT COUNT(*) FROM "+DB_TABLE+" WHERE accept_connect= TRUE AND bot_name=%s;", (self.bot_name,))
+        self.cur.execute("SELECT COUNT(*) FROM "+DB_TABLE+" WHERE accept_connect=1 AND bot_name=?;", (self.bot_name,))
         all_count = self.cur.fetchone()
-        self.cur.execute("SELECT COUNT(*) FROM "+DB_TABLE+" WHERE accept_connect= TRUE AND accept_date >=DATE('now') AND bot_name=%s;",(self.bot_name,))
+        self.cur.execute("SELECT COUNT(*) FROM "+DB_TABLE+" WHERE accept_connect=1 AND accept_date >=DATE('now') AND bot_name=?;", (self.bot_name,))
         today_count = self.cur.fetchone()
         self.con.close()
         return today_count, all_count
 
-   # @db_decorator
     def candidate_for_forward(self):
-        self.cur.execute("SELECT name FROM "+DB_TABLE+" WHERE send_message=TRUE AND second_message= FALSE AND finished= FALSE AND send_date < current_date - interval '5' day AND bot_name=%s;", (self.bot_name,))
+        self.cur.execute("SELECT name FROM "+DB_TABLE+" WHERE send_message=1 AND second_message=0 AND finished=0 AND send_date < current_date - interval '5' day AND bot_name=?;", (self.bot_name,))
         cand = self.cur.fetchall()
         self.con.close()
         return cand
 
-   # @db_decorator
     def finish(self, name):
-        query = "UPDATE "+DB_TABLE+" SET finished= TRUE WHERE name=%s AND bot_name=%s;"
+        query = "UPDATE "+DB_TABLE+" SET finished=1 WHERE name=? AND bot_name=?;"
         self.cur.execute(query, (name, self.bot_name,))
         self.con.commit()
         self.con.close()
 
-   # @db_decorator
     def send_second_message(self, name):
-        query = "UPDATE "+DB_TABLE+" SET finished= TRUE, second_message= TRUE, second_message_date=%s WHERE name=%s AND bot_name=%s;"
+        query = "UPDATE "+DB_TABLE+" SET finished=1, second_message=1, second_message_date=? WHERE name=? AND bot_name=?;"
         self.cur.execute(query, (date.today(), name, self.bot_name,))
         self.con.commit()
         self.con.close()
